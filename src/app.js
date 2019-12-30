970d9530d7593906b66133a9a831db937a3e7eaf
triciamedina <tricia.medina@gmail.com>
2019-12-30 07:22:44 -0800

    setup router for /user

--- src/app.js
+++ src/app.js
@@ -1,80 +1,38 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
-const winston = require('winston');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const userRouter = require('./user/user-router')
 
-const app = express();
+const app = express()
 
 const morganOption = (NODE_ENV === 'production')
     ? 'tiny'
     : 'common';
 
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports: [
-        new winston.transports.File({ filename: 'info.log'})
-    ]
-});
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
 
-if (NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }))
-}
-
-app.use(morgan(morganOption));
-app.use(helmet());
-app.use(cors());
-
-app.use(express.json());
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    // move to the next middleware
+    next()
+})
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
-});
+})
 
-app.post('/user', (req, res) => {
-    const { email, firstName, lastName, password } = req.body;
-    if (!email) {
-        return res
-            .status(400)
-            .send('Email required')
-    }
-    if (email.length < 6 || email.length > 50) {
-        return res
-            .status(400)
-            .send('Email must be between 6 and 50 characters')
-    }
-    if (!firstName) {
-        return res
-            .status(400)
-            .send('First name required')
-    }
-    if (!lastName) {
-        return res
-            .status(400)
-            .send('Last name required')
-    }
-    if (!password) {
-        return res
-            .status(400)
-            .send('Password required')
-    }
-    if (password.length < 8 || password.length > 36) {
-        return res
-            .status(400)
-            .send('Password must be between 8 and 36 characters')
-    }
-    if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
-        return res
-            .status(400)
-            .send('Password must contain at least one digit')
-    }
-    res.send('All validation passed')
-});
+app.use(userRouter)
 
 app.use(function errorHandler(error, req, res, next) {
     let response;
@@ -85,6 +43,6 @@ app.use(function errorHandler(error, req, res, next) {
         response = { message: error.message, error }
     }
     res.status(500).json(response);
-});
+})
 
-module.exports = app;
+module.exports = app
---
+++ src/store.js
@@ -0,0 +1,11 @@
+const users = [
+    {
+        id: 1,
+        email: 'email@email.com',
+        firstName: 'Sam',
+        lastName: 'Gamgee',
+        password: 'password',
+    }
+]
+
+module.exports = { users }

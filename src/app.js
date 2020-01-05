dd34914a137c127c06670d09edc4525e81673502
triciamedina <tricia.medina@gmail.com>
2020-01-05 15:48:15 -0800

    prepare server for deployment

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config')
 const userRouter = require('./user/user-router')
 const authRouter = require('./auth/auth-router')
 const charityRouter = require('./charity/charity-router')
@@ -17,6 +17,10 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}))
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');

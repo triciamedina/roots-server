6f8333e37ee0258b761240a7bd291edf948e1c54
triciamedina <tricia.medina@gmail.com>
2020-01-05 21:41:07 -0800

    update CLIENT_ORIGIN

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql:roots@localhost/roots',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000/',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     CHARITY_URLS: ['https://api.donorschoose.org/common/json_feed.html'],
     CHARITY_API_KEY: process.env.CHARITY_API_KEY || 'change-this-key',

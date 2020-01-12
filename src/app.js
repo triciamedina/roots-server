3d6e2893a09b6d0fc77d8d20adc6dfea8ea96edf
triciamedina <tricia.medina@gmail.com>
2020-01-12 12:56:03 -0800

    troubleshoot cors

--- src/app.js
+++ src/app.js
@@ -18,9 +18,9 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.use(cors({
-    origin: CLIENT_ORIGIN
-}))
+// app.use(cors({
+//     origin: CLIENT_ORIGIN
+// }))
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
